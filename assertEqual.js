d8f9286842c9812b7a878816b32f9682dd74ef1d
Edison Yang <edison.cy.yang@gmail.com>
2019-11-25 22:33:53 +0000

    function to determine if two inputs are equal

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("💯💯💯Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌❌❌Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("javascript", "javascript");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(5, 50);
