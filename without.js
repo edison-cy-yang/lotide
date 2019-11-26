36208a4a21e784ca9572ca1bf268e3f2dee2c0f0
Edison Yang <edison.cy.yang@gmail.com>
2019-11-26 22:39:18 +0000

    implemented without function to return a subset of the array while removing unwanted items

---
+++ without.js
@@ -0,0 +1,39 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i])
+        return false;
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💯💯💯Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  let filtered = [];
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      filtered.push(source[i]);
+    }
+  }
+  return filtered;
+};
+
+const numberArray = [1,2,3,4,5];
+let filteredArray = without(numberArray, [3, 4]);
+
+assertArraysEqual(eqArrays(filteredArray, [1,2,5]), true);
+assertArraysEqual(eqArrays(filteredArray, [1,2]), false);
+
+const words = ["hello", "world", "lighthouse"];
+without(["hello", "world", "lighthouse"], ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(eqArrays(words, ["hello", "world", "lighthouse"]), true);
