b1af7ac9950c342ab7715f0ff31e7103abdbd2fa
Edison Yang <edison.cy.yang@gmail.com>
2019-11-27 20:14:40 +0000

    implemented function to find the key of given oobject, if there exists a key with a matching value

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💯💯💯Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  let result;
+  for(let key in object) {
+    if (object[key] === value) {
+      result = key;
+    }
+  }
+  return result;
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
