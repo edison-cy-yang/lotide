bc792b33a1ba01faade05cee338cb4061c5b2bf7
Edison Yang <edison.cy.yang@gmail.com>
2019-11-30 22:56:51 +0000

    bug fix

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -26,7 +26,7 @@ const eqArrays = function(arr1, arr2) {
   }
 };
 
-const eqObjects = function(object1, object2) {
+const eqObjects1 = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length)
     return false;
   for (let key in object1) {
@@ -39,6 +39,33 @@ const eqObjects = function(object1, object2) {
         return false;
     } else if (!Array.isArray(object1[key]) && !Array.isArray(object2[key])) {
       if (!eqObjects(object1[key], object2[key])) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length)
+    return false;
+  for (let key in object1) {
+    if (object2[key] === undefined)
+      return false;
+    else if(object1[key] !== object2[key]) {
+      //if two values are not equal there could be two possibilities:
+      //1. both are arrays and they are actually equal arrays
+      //2. both are objects and they are actually equal objects
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      } else if (!Array.isArray(object1[key]) && !Array.isArray(object2[key])) {
+        if (!eqObjects(object1[key], object2[key])) {
+          return false;
+        }
+      }
+      else {
         return false;
       }
     }
