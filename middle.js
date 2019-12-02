f3ab2db27e50e708a44c2e8e865a7d85e5dc1492
Edison Yang <edison.cy.yang@gmail.com>
2019-12-02 18:39:47 +0000

    refactored middle and its test code

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i])
-        return false;
-    }
-    return true;
-  }
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`💯💯💯Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const middle = function(array) {
   let midElements = [];
   if (array.length <= 2) return midElements;
@@ -31,7 +11,4 @@ const middle = function(array) {
   return midElements;
 };
 
-assertArraysEqual(eqArrays(middle([1,2,3]), [2]), true);
-assertArraysEqual(eqArrays(middle([1,2,3,4,5]), [3]), true);
-assertArraysEqual(eqArrays(middle([1,2,3,4,5,6]), [3,4]), true);
-console.log(middle([1,2]));
+module.exports = middle;
